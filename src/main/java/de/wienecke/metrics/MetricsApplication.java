6693660e40b1dbf9df96ffc0aab4e6b385b8a3e0
Thomas Wienecke <wienecke.t@gmail.com>
2017-07-29 15:37:09 +0200

    Initial commit

---
+++ src/main/java/de/wienecke/metrics/MetricsApplication.java
@@ -0,0 +1,12 @@
+package de.wienecke.metrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetricsApplication.class, args);
+	}
+}
